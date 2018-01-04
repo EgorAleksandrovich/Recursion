eeb8c108cb5b4ca9ccc158e443687dd6188f8c68
EgorAleksandrovich <egorkamolodec2015@gmail.com>
2018-01-04 15:28:44 +0200

    Some recursion tasks. MergeSort.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        public static int[] Sort(int[] arrayForSort)
+        {
+            if (arrayForSort.Length == 1)
+            {
+                return arrayForSort;
+            }
+            int[] firsArray;
+            int[] secondArray;
+            Split(arrayForSort, out firsArray, out secondArray);
+
+            return Merge(Sort(firsArray), Sort(secondArray));
+        }
+
+        public static int[] Merge(int[] firstArray, int[] secondArray)
+        {
+            int arrayLength = firstArray.Length + secondArray.Length;
+            int[] resultArray = new int[arrayLength];
+            int countFirstArray = 0;
+            int countSecondArray = 0;
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                if (firstArray.Length == countFirstArray)
+                {
+                    resultArray[i] = secondArray[countSecondArray];
+                    countSecondArray++;
+                }
+                else
+                {
+                    if (secondArray.Length == countSecondArray)
+                    {
+                        resultArray[i] = secondArray[countFirstArray];
+                        countFirstArray++;
+                    }
+                    else
+                    {
+                        if (firstArray[countFirstArray] < secondArray[countSecondArray])
+                        {
+                            resultArray[i] = firstArray[countFirstArray];
+                            countFirstArray++;
+                        }
+                        else
+                        {
+                            if (firstArray[countFirstArray] == secondArray[countSecondArray])
+                            {
+                                resultArray[i] = firstArray[countFirstArray];
+                                i++;
+                                resultArray[i] = firstArray[countSecondArray];
+                                countFirstArray++;
+                                countSecondArray++;
+                            }
+                            else
+                            {
+                                resultArray[i] = secondArray[countSecondArray];
+                                countSecondArray++;
+                            }
+                        }
+                    }
+                }
+            }
+            return resultArray;
+        }
+
+        public static void Split<T>(T[] array, out T[] first, out T[] second)
+        {
+            int index = array.Length / 2;
+            first = array.Take(index).ToArray();
+            second = array.Skip(index).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 6, 3, 9, 9 };
+            Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ recursion/Recursion.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    public class Recursion
+    {
+        public static int recursion1(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            Console.WriteLine(n);
+            return recursion1(n - 1) * n;
+        }
+
+        public static string recursion2(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+            return recursion2(n - 1) + ", " + n;
+        }
+
+        public static string recursion3(int a, int b)
+        {
+            if (b == a)
+            {
+                return b.ToString();
+            }
+            if (a > b)
+            {
+                return recursion3(a, b + 1) + ", " + b;
+            }
+            else
+            {
+                return recursion3(a, b - 1) + ", " + b;
+            }
+        }
+
+        public static string recursion4(int a)
+        {
+            if (a == 1)
+            {
+                return "YES";
+            }
+            if (a % 2 != 0)
+            {
+                return "NOT";
+            }
+            return recursion4(a / 2);
+        }
+
+        public static int recursion5(int a)
+        {
+            if (a < 10)
+            {
+                return a;
+            }
+            return recursion5(a / 10) + a % 10;
+        }
+
+        public static string recursion6(int a, int b)
+        {
+            if (b == a || a == 2 || b == 1)
+            {
+                return "YES";
+            }
+            if (a % b == 0 )
+            {
+                return "NO";
+            }
+            return recursion6(a, b - 1);
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Task 1\n");
+            Console.WriteLine(recursion1(5));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Task 2\n");
+            Console.WriteLine(recursion2(5));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Task 3\n");
+            Console.WriteLine(recursion3(11, 6));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Task 4\n");
+            Console.WriteLine(recursion4(6));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Task 5\n");
+            Console.WriteLine(recursion5(6615));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Task 5\n");
+            int a = 4;
+            Console.WriteLine(recursion6(a, 2));
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
